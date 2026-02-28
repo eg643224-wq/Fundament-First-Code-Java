6065d37cdec724406ff4bea2945705746e8fd14b
eg643224-wq <eg643224@gmail.com>
2026-02-27 21:23:30 -0600

    Add README file

--- src/requestDataAndDisplay.java
+++ src/requestDataAndDisplay.java
@@ -1,2 +1,18 @@
+
+import java.util.Scanner;
 public class requestDataAndDisplay {
+    public static void main (String[] args){
+        System.out.print ("Cual es tu nombre?: ");
+        Scanner scanner=new Scanner (System.in);
+        String nombre=scanner.nextLine();
+        System.out.println("Cual es tu edad?: ");
+        int edad=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("En que dia naciste?: ");
+        String nacimiento= scanner.nextLine();
+        System.out.println("Tu nombre es: "+nombre);
+        System.out.println("Tienes: "+edad);
+        System.out.print("Naciste en: "+nacimiento);
+
+    }
 }
